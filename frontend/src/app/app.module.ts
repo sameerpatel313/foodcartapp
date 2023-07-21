1c40705e9fc273d258d4ee96c8ab59122514a05e
sameerpatel313 <131300412+sameerpatel313@users.noreply.github.com>
2023-07-21 20:52:19 +0530

    '#'

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxStarsModule } from 'ngx-stars';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxStarsModule,
+
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
